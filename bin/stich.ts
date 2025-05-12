9425bb7cf050cd946bd05e5ea5198678c0903594
hakenshi <fkdias025@gmail.com>
2025-05-12 07:38:46 -0300

    Adiciona comando 'init' para scaffolding de novos projetos NextJS ou Vite

--- bin/stich.ts
+++ bin/stich.ts
@@ -5,11 +5,25 @@ const program = new Command()
 
 program.name("stich").description("Stich CLI - Nextjs and React development tools").version("0.1.0")
 
+program.command('init <project>')
+    .description("Helps you caffold a new NextJS or a new vite with react project.")
+    .option("--template", "template", 'next')
+    .option('--with-orm', 'orm', '')
+    .option("--with-prettier", 'withPrettier', '')
+    .action((project, options) => {
+        if(options.template){
+            
+        }       
+    })    
+
 program.command("make:component <name>")
     .description("Creates an empty react component")
     .option('--with-children', "withChildren", '')
     .option('--with-props', "withProps", '')
     .option('--with-styles', "withStyles", '')
     .action(makeComponent)
+
+program.command('deploy')
+    .description('Deploys this project to vercel.')
 
 program.parse(process.argv)
